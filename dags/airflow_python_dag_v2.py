49d05132607097aab6069cef09323b887e08e81c
SaberHardy <samirsamado5@gmail.com>
2025-05-24 11:31:11 +0100

    add dag to insert into postgres db

--- dags/airflow_python_dag_v2.py
+++ dags/airflow_python_dag_v2.py
@@ -3,44 +3,40 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from datetime import datetime, timedelta
 
-
 default_args = {
     'owner': 'coder2j',
     'retries': 5,
     'retry_delay': timedelta(minutes=5)
 }
 
-
-with DAG(
-    dag_id='dag_with_postgres_operator_v03',
-    default_args=default_args,
-    start_date=datetime(2021, 12, 19),
+with DAG(dag_id='dag_with_postgres_operator_v3',
+        default_args=default_args,
+        start_date=datetime(2021, 12, 19),
 ) as dag:
     task1 = SQLExecuteQueryOperator(
         task_id='create_postgres_table',
         conn_id='postgres_localhost',
-        sql="""
-            create table if not exists dag_runs (
+        sql="""create table if not exists dag_runs (
                 dt date,
                 dag_id character varying,
-                primary key (dt, dag_id)
-            )
-        """
+                primary key (dt, dag_id))
+            """
     )
 
     task2 = SQLExecuteQueryOperator(
-        task_id='insert_into_table',
-        conn_id='postgres_localhost',
-        sql="""
-            insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
-        """
-    )
-
-    task3 = SQLExecuteQueryOperator(
         task_id='delete_data_from_table',
         conn_id='postgres_localhost',
         sql="""
-            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}';
-        """
+                delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}';
+            """
     )
-    task1 >> task3 >> task2
+
+    task3 = SQLExecuteQueryOperator(
+        task_id='insert_into_table_300',
+        conn_id='postgres_localhost',
+        sql="""
+                insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
+            """
+    )
+
+    task1 >> task2 >> task3
