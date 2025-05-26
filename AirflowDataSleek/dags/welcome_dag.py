fe91b1eecf0c37190f4865ff4e1ca7ac8e8d1c43
SaberHardy <samirsamado5@gmail.com>
2025-05-26 10:21:54 +0100

    use airflow variables

--- AirflowDataSleek/dags/welcome_dag.py
+++ AirflowDataSleek/dags/welcome_dag.py
@@ -1,6 +1,7 @@
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from airflow.sdk import Variable
 from airflow import DAG
 
 from helpers.clean_file import clean_data
@@ -11,15 +12,23 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
+try:
+    data_url = Variable.get("data_url")
+    print(f"Data link is: {repr(data_url)}")
+except Exception as e:
+    print("This is an exception")
+    raise
+
+
 # Define or Instantiate DAG
-with DAG(dag_id='exchange_rate_etl_gcp_v06',
+with DAG(dag_id='exchange_rate_etl_gcp_v09',
          start_date=datetime(2025, 5, 10, 1),
          default_args=default_args,
          catchup=False) as dag:
 
     download_task = BashOperator(
         task_id='download_file',
-        bash_command='curl -o /tmp/xrate.csv https://data-api.ecb.europa.eu/service/data/EXR/M.USD.EUR.SP00.A?format=csvdata',
+        bash_command=f'curl -o /tmp/xrate.csv {data_url}',
     )
 
     clean_data_task = PythonOperator(
